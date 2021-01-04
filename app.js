7ae88c6f00882dd25c7f35c1ebc4688643014703
Ulibka68 <gena68@inbox.ru>
2021-01-04 22:50:47 +0300

    4 Циклы с v-for   вар 2 - объект

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 Vue.createApp({
     data : () => ({
         title : 'Заголовок 3',
-        myHtml: '<h1>Vue 3 APP</h1>'
+        myHtml: '<h1>Vue 3 APP</h1>',
+        person: {
+            firstName : 'Гайрат',
+            lastName : 'Власов',
+            age : 26
+        }
     })
 }).mount("#app");
