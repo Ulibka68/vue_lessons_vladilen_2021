cc5e7ee0f9e7308bd8fdc2d7c05d46c601417f9e
Ulibka68 <gena68@inbox.ru>
2021-01-04 12:51:19 +0300

    7 Пример. Создаем двустороннее связывание

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const App = {
     data() {
         return {
             placeholderString : "Введите заметку",
-            title : 'Список заметок'
+            title : 'Список заметок',
+            inputValue: ''
         }
+    },
+    methods: { // набор методов в HTML
+        inputChangeHandle(event){
+            this.inputValue = event.target.value;
+        }
+
     }
 }
 Vue.createApp(App).mount("#app");
+
