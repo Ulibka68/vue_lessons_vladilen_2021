580f8afe7db31d3cd3b33f77bb0d074a1f02a3b0
Ulibka68 <gena68@inbox.ru>
2021-01-05 08:34:46 +0300

    2 Virtual DOM и render

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-Vue.createApp({
+const h = Vue.h;
+
+const app = Vue.createApp({
     data() {
         return {
             title: 'data title'
         }
     },
-    template: `
-    <div class="card center">
-        <h1>Это из свойства template {{title}}</h1>
-        <button class="btn"  @click="title='новый title'" >Изменить</button>
-    </div>
-    `
-}).mount('#app');
+    render() {
+        return h('div',{class : 'card center'},
+            [
+                h('h1',{},'h1 text '+this.title),
+                h('button',
+                    {class: 'btn', onclick: this.changeTitle},
+                    'Изменить')
+            ]);
+    },
+    methods: {
+        changeTitle() {
+            this.title = 'changeTitle';
+        }
+    }
+});
+
+app.mount('#app');
+//
+// `
+//     <div class="card center">
+//         <h1>Это из свойства template {{title}}</h1>
+//         <button class="btn"  @click="title='новый title'" >Изменить</button>
+//     </div>
+//     `)
