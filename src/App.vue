38f4aabe0b0199337fff65b7b14c2e460ae1496d
Ulibka68 <gena68@inbox.ru>
2021-01-10 06:19:58 +0300

    3 Зачем использовать компоненты?

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Актуальные новости {{ now }}</h2>
+    </div>
+    <hr />
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{ item }}</h3>
+      <button class="btn" @click="isopen = !isopen">Открыть</button>
+      <p v-if="isopen">Lorem ipsum dolor sit amet.</p>
+    </div>
   </div>
 </template>
 
@@ -8,6 +16,13 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      isopen: false,
+      now: new Date().toLocaleDateString(),
+      news: ["Джо байден выборы США", "Vue 3 работает"],
+    };
+  },
 };
 </script>
 
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,10 @@
 <template>
   <h2>{{ title }}</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex maiores
+    possimus quasi quia repellat repudiandae sequi velit veniam voluptate
+    voluptatem.
+  </p>
 </template>
 
 <script>
