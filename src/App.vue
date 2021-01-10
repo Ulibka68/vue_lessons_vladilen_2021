9387d701562c4ce202ddfb35c60a4825be131be6
Ulibka68 <gena68@inbox.ru>
2021-01-10 06:55:16 +0300

    6 Валидируем параметры

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
       <h2>Актуальные новости {{ now }}</h2>
     </div>
 
-    <AppNews v-for="item in news" :key="item" :title="item" />
+    <AppNews
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-open="item.isOpen"
+    />
     <hr />
   </div>
 </template>
@@ -17,7 +23,10 @@ export default {
   data() {
     return {
       now: new Date().toLocaleDateString(),
-      news: ["Джо байден выборы США", "Vue 3 работает"],
+      news: [
+        { title: "Джо байден выборы США", id: 1, isOpen: false },
+        { title: "Vue 3 работает", id: 2, isOpen: false },
+      ],
     };
   },
   components: {
--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="card">
     <h3>{{ title }}</h3>
-    <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+    <!--    <button class="btn" @click="isOpen = !isOpen">Открыть</button>-->
+    <button class="btn">Открыть</button>
     <p v-if="isOpen">Lorem ipsum dolor sit amet.</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["title"],
+  // props: ["title"],
+  props: {
+    title: { type: String, required: true },
+    id: Number, // Proxy, Array, Promise
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator(value) {
+        return value === true;
+      },
+    },
+  },
   data() {
-    return { isOpen: false };
+    // return { isOpen: false };
   },
 };
 </script>
