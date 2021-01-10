4754b25b34cca3c1329d92f6e228f8aca762c498
Ulibka68 <gena68@inbox.ru>
2021-01-10 06:30:04 +0300

    4 Создаем отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,26 @@
     <div class="card">
       <h2>Актуальные новости {{ now }}</h2>
     </div>
+    <app-news></app-news>
     <hr />
-    <div class="card" v-for="item in news" :key="item">
-      <h3>{{ item }}</h3>
-      <button class="btn" @click="isopen = !isopen">Открыть</button>
-      <p v-if="isopen">Lorem ipsum dolor sit amet.</p>
-    </div>
   </div>
 </template>
 
 <script>
+import AppNews from "@/components/AppNews";
+
 export default {
   name: "App",
-  components: {},
   data() {
     return {
       isopen: false,
       now: new Date().toLocaleDateString(),
       news: ["Джо байден выборы США", "Vue 3 работает"],
     };
+  },
+  components: {
+    // "app-news": AppNews,
+    AppNews,
   },
 };
 </script>
---
+++ src/components/AppNews.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h3>{{ item }}</h3>
+    <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+    <p v-if="isOpen">Lorem ipsum dolor sit amet.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { item: "Items string", isOpen: false };
+  },
+};
+</script>
