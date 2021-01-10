f306127bf1906938524f40cffe9a960e40a97807
Ulibka68 <gena68@inbox.ru>
2021-01-10 15:34:20 +0300

    13 Как работают slots

---
+++ src/components/AppBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <slot name="default"> No text выводится если нет children </slot>
+    <div v-if="$slots.footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBlock",
+};
+</script>
+
+<style scoped></style>
