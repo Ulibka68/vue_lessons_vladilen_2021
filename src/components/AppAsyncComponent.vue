db0106149ad4ca30c47ea3ebedc34a961f11a7b9
Ulibka68 <gena68@inbox.ru>
2021-01-10 22:52:29 +0300

    19 Асинхронные компоненты

---
+++ src/components/AppAsyncComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Async Component d384aa</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AsyncComponent",
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from "vue";
+import { createApp, defineAsyncComponent } from "vue";
 import App from "./App.vue";
 import TheHeader from "./components/TheHeader";
 import "./theme.css";
 
 const app = createApp(App);
 app.component("the-header", TheHeader);
+app.component(
+  "async-component",
+  defineAsyncComponent(() => {
+    return import("./components/AppAsyncComponent");
+  })
+);
 app.mount("#app");
