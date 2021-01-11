743bb30770cda9989bee5dce004d24e0454e4719
Ulibka68 <gena68@inbox.ru>
2021-01-11 08:25:58 +0300

    8 Vue и работа с HTTP   1 Готовим приложение с формой

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2>Работа с базой данных</h2>
+      <form class="card" @submit.prevent="createPerson">
+        <h2>Работа с базой данных</h2>
+        <div class="form-control">
+          <label for="name"> Введите имя </label>
+          <input type="text" id="name" v-model.trim="name" />
+        </div>
+        <button class="btn primary" :disabled="!name">Отправить</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return { name: "" };
+  },
+  methods: {
+    createPerson() {
+      console.log(this.name);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
