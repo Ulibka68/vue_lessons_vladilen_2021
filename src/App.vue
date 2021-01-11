8cf03377e5b9e353c696c0f3105c5bc8c8a20608
Ulibka68 <gena68@inbox.ru>
2021-01-11 07:26:38 +0300

    7 Работа с элементами формы   валидация

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
-    <form class="card">
+    <form class="card" @submit.prevent="formSubmit">
       <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: errors.name }">
         <label for="name">Как тебя зовут?</label>
-        <input type="text" id="name" placeholder="Введи имя">
+        <input
+          v-model.trim="name"
+          type="text"
+          id="name"
+          placeholder="Введи имя"
+        />
+        <small v-if="errors.name">{{ errors.name }}</small>
       </div>
 
       <div class="form-control">
         <label for="age">Выбери возраст</label>
-        <input type="number" id="age" value="20">
+        <input type="number" id="age" value="20" />
       </div>
 
       <div class="form-control">
@@ -25,24 +31,24 @@
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Да</label>
+          <label><input type="radio" name="trip" /> Да</label>
         </div>
 
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Нет</label>
+          <label><input type="radio" name="trip" /> Нет</label>
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vuex</label>
+          <label><input type="checkbox" /> Vuex</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue CLI</label>
+          <label><input type="checkbox" /> Vue CLI</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue Router</label>
+          <label><input type="checkbox" /> Vue Router</label>
         </div>
       </div>
 
@@ -52,9 +58,34 @@
 </template>
 
 <script>
-  export default {}
+export default {
+  data() {
+    return { name: "", errors: { name: null } };
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    formSubmit(event) {
+      if (this.isValid()) {
+        console.log(this.name);
+      }
+    },
+    isValid() {
+      if (!this.name) {
+        this.errors.name = "Введите имя";
+        return false;
+      }
+      this.errors.name = null;
+      return true;
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.form-control small {
+  color: #c25205;
+}
+.form-control.invalid input {
+  border-color: red;
+}
 </style>
