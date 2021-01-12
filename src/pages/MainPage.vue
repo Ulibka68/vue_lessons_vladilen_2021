0b1a29e3e9350013483228b6e7819e3dcb294557
Ulibka68 <gena68@inbox.ru>
2021-01-12 20:50:53 +0300

    Курсовая 2. Приложение "Генерация Резюме"   сделана кнопка запись в бд

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="container column">
-    <form class="card card-w30">
-      <div class="form-control">
-        <label for="type">Тип блока</label>
-        <select id="type">
-          <option value="title">Заголовок</option>
-          <option value="subtitle">Подзаголовок</option>
-          <option value="avatar">Аватар</option>
-          <option value="text">Текст</option>
-        </select>
-      </div>
+    <div class="card card-w30">
+      <form>
+        <div class="form-control">
+          <label for="type">Тип блока</label>
+          <select id="type">
+            <option value="title">Заголовок</option>
+            <option value="subtitle">Подзаголовок</option>
+            <option value="avatar">Аватар</option>
+            <option value="text">Текст</option>
+          </select>
+        </div>
 
-      <div class="form-control">
-        <label for="value">Значение</label>
-        <textarea id="value" rows="3"></textarea>
-      </div>
+        <div class="form-control">
+          <label for="value">Значение</label>
+          <textarea id="value" rows="3"></textarea>
+        </div>
 
-      <button class="btn primary">Добавить</button>
-    </form>
+        <button class="btn primary">Добавить</button>
+      </form>
 
+      <button class="btn primary mg-1 fit-width">
+        Записать изменения в базу данных
+      </button>
+    </div>
     <div class="card card-w70">
       <component
         :is="item.blockType"
@@ -57,45 +62,12 @@ import ResumeHeader from "@comp/resume-blocks/ResumeHeader";
 import ResumeAvatar from "@comp/resume-blocks/ResumeAvatar";
 import ResumeSubHeader from "@comp/resume-blocks/ResumeSubHeader";
 import ResumeText from "@comp/resume-blocks/ResumeText";
+import { MainPageData } from "./MainPageData"; // временные тестовые данные
 
 export default {
   data() {
     return {
-      blocks: [
-        {
-          id: 1,
-          blockType: "ResumeHeader",
-          text: "Резюме Nickname",
-        },
-        {
-          id: 2,
-          blockType: "ResumeAvatar",
-          text:
-            "https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png",
-        },
-        {
-          id: 3,
-          blockType: "ResumeSubHeader",
-          text: "Опыт работы",
-        },
-        {
-          id: 4,
-          blockType: "ResumeText",
-          text:
-            "главный герой американского мультсериала «Рик и Морти», гениальный\n" +
-            "        учёный, изобретатель, атеист (хотя в некоторых сериях он даже молится\n" +
-            "        Богу, однако, каждый раз после чудесного спасения ссылается на удачу и\n" +
-            "        вновь отвергает его существование), алкоголик, социопат, дедушка Морти.\n" +
-            "        На момент начала третьего сезона ему 70 лет[1]. Рик боится пиратов, а\n" +
-            '        его главной слабостью является некий - "Санчезиум". Исходя из того, что\n' +
-            "      существует неограниченное количество вселенных, существует\n" +
-            "      неограниченное количество Риков, герой сериала предположительно\n" +
-            "      принадлежит к измерению С-137. В серии комикcов Рик относится к\n" +
-            "      измерению C-132, а в игре «Pocket Mortys» — к измерению C-123[2].\n" +
-            "      Прототипом Рика Санчеза является Эмметт Браун, герой кинотрилогии «Назад\n" +
-            "      в будущее»[3].",
-        },
-      ],
+      blocks: MainPageData,
     };
   },
   created() {
