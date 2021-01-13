2451469c95cebe65a94d3596d545035241df7882
Ulibka68 <gena68@inbox.ru>
2021-01-13 14:25:53 +0300

    Курсовая 2. Приложение "Генерация Резюме"   косметические правки

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,59 +1,38 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Login</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right"
-                  >Email</label
-                >
+    <h1>Login</h1>
+    <div class="card card-w70">
+      <div v-if="error" class="alert danger">{{ error }}</div>
+      <form action="#" @submit.prevent="submit">
+        <div class="form-control">
+          <label for="email">Email</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label
-                  for="password"
-                  class="col-md-4 col-form-label text-md-right"
-                  >Password</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-              </div>
-            </form>
-          </div>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value
+            required
+            autofocus
+            v-model="form.email"
+          />
         </div>
-      </div>
+
+        <div class="form-control">
+          <label for="password">Password</label>
+
+          <input
+            id="password"
+            type="password"
+            class="form-control"
+            name="password"
+            required
+            v-model="form.password"
+          />
+        </div>
+
+        <button type="submit" class="btn primary">Login</button>
+      </form>
     </div>
   </div>
 </template>
