b3257947752940f618b367276db30de3215a7503
Ulibka68 <gena68@inbox.ru>
2021-01-14 10:33:24 +0300

    добавил typescript + router

---
+++ fbconf.example.js
@@ -0,0 +1,10 @@
+// необходимо получить настройки в консоли
+export const firebaseConfig = {
+  apiKey: "AIzaSyBp33----------",
+  authDomain: "vue-vladilen.firebaseapp.com",
+  databaseURL: "https://vue-vladilen-4c695-default-rtdb.firebaseio.com",
+  projectId: "vue-vladilen",
+  storageBucket: "vue-vladilen.appspot.com",
+  messagingSenderId: "90318077999999",
+  appId: "1:903222:web:2e9dc",
+};
