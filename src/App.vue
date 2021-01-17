445880b0db30fce30e934e2cb10d1f97b1601f1d
Ulibka68 <gena68@inbox.ru>
2021-01-18 01:40:55 +0300

    9 Миксины, директивы и плагины   модальное окно открывается/закрывается

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2>{{ $i18n('app.title') }}</h2>
+      <h2>{{ $i18n("app.title") }}</h2>
 
-      <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn') }}</button>
+      <button class="btn" @click="changeLang">
+        {{ $i18n("app.changeBtn") }}
+      </button>
       <button class="btn primary" @click="modal = true">Открыть модалку</button>
 
-      <teleport to="#modal">
-        <app-modal
-          v-if="modal"
-          @close="modal = false"
-        ></app-modal>
-      </teleport>
+      <app-modal v-if="modal" @close="modal = false"></app-modal>
     </div>
   </div>
 </template>
 
 <script>
-import AppModal from './components/AppModal'
+import AppModal from "./components/AppModal";
 
 export default {
-  inject: ['changeI18N'],
+  inject: ["changeI18N"],
   data() {
     return {
-      modal: false
-    }
+      modal: false,
+    };
   },
   methods: {
     changeLang() {
-      this.changeI18N('en')
-      this.$forceUpdate()
-    }
+      this.changeI18N("en");
+      this.$forceUpdate();
+    },
   },
-  components: {AppModal}
-}
+  components: { AppModal },
+};
 </script>
-
--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -2,16 +2,18 @@
   <div class="modal-backdrop" @click="$emit('close')"></div>
   <div class="modal">
     <h3>Это заголовок модалки</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi excepturi inventore perferendis quaerat quam repudiandae unde veritatis vero voluptates?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi
+      excepturi inventore perferendis quaerat quam repudiandae unde veritatis
+      vero voluptates?
+    </p>
   </div>
 </template>
 
 <script>
-  export default {
-    emits: ['close']
-  }
+export default {
+  emits: ["close"],
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
