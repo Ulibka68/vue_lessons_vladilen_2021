49325be9b8e388b86f774c913a85c1d6edee5402
Ulibka68 <gena68@inbox.ru>
2021-01-18 21:14:08 +0300

    11 Vuex - Работа с данными   2 Начинаем работу со Store     store инициализирован

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик {{ counter }}</h2>
+      <h2>Счетчик {{ $store.state.counter }}</h2>
       <button class="btn" @click="increment">Добавить</button>
     </div>
   </div>
@@ -13,14 +13,9 @@
 import TheNavbar from "./TheNavbar";
 export default {
   components: { TheNavbar },
-  data() {
-    return {
-      counter: 0,
-    };
-  },
   methods: {
     increment() {
-      this.counter++;
+      this.$store.state.counter++;
     },
   },
 };
