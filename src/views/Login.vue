f1419dfbd989271f1caa5fb2aee43cb51a6354d4
Ulibka68 <gena68@inbox.ru>
2021-01-18 03:13:31 +0300

    10 Vue Router   4 Динамическая навигация     разные примеры применения router link

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,6 +13,18 @@
 
     <button class="btn primary" type="submit">Войти</button>
     <button class="btn warning">Забыл пароль?</button>
+
+    <!--    <br />
+    <a href="/forget">Забыл пароль</a><br />
+    <router-link to="/forget">Переход на Forget без перезагрузки</router-link>-->
+
+    <router-link to="/forget" class="btn warning"
+      >Забыл пароль - router</router-link
+    >
+    <!--    добавляем слот к router link-->
+    <router-link to="/forget">
+      <button class="warning btn">Забыл пароль button slot</button>
+    </router-link>
   </form>
 </template>
 
