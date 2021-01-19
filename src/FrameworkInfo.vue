69db937cdd2a9edc2f0b9ef2069a6977518d0d2c
Ulibka68 <gena68@inbox.ru>
2021-01-19 21:43:38 +0300

    12 Работа с Composition API   9 Взаимодействие компонентов     FrameworkInfo.vue - composition компонент

--- src/FrameworkInfo.vue
+++ src/FrameworkInfo.vue
@@ -4,16 +4,23 @@
       Название: <strong>{{ name }}</strong>
     </p>
     <p>
-      Версия: <strong>{{ version }}</strong>
+      Версия: <strong>{{ version }}</strong> - ({{ doubleVer }})
     </p>
   </div>
 </template>
 
 <script>
+import { computed } from "vue";
+
 export default {
   name: "FrameworkInfo",
   props: ["name", "version"],
-  computed: {},
+  setup(props) {
+    const doubleVer = computed(() => props.version * 2);
+    return {
+      doubleVer,
+    };
+  },
 };
 </script>
 
