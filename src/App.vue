940c85794476e24ed11a5af1a5fd31a6a32b207d
Ulibka68 <gena68@inbox.ru>
2021-01-19 21:38:32 +0300

    12 Работа с Composition API   9 Взаимодействие компонентов     FrameworkInfo.vue - обычный компонент

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,15 @@
     <div class="card">
       <h1>Vue Composition Api</h1>
       <hr />
-      <p>
-        Название: <strong>{{ framework2.name2 }}</strong>
-      </p>
-      <p>
-        Версия: <strong>{{ framework2.version2 }}</strong>
-      </p>
-      <p>name: {{ name }} + version: {{ version }}</p>
-      <p>name2: {{ framework2.name2 }} + name3: {{ name3 }}</p>
-      <p>doubleVer: {{ doubleVer }}</p>
-      <p>doubleFrame: {{ doubleFrame }}</p>
 
       <div class="form-control">
         <input type="text" ref="textInput" />
       </div>
 
-      <br />
-      <p>input v-model="firstName" /></p>
-      <div class="form-control">
-        <input type="text" v-model="firstName" />
-      </div>
-      <p>input v-model="formInpust" /></p>
-      <div class="form-control">
-        <input type="text" v-model="formInputs.lName" />
-      </div>
-
       <button class="btn" @click="change">Изменить</button>
-      <button class="btn" @click="handleInpVal">Получить значение input</button>
     </div>
+
+    <FrameworkInfo :name="name" :version="version" />
   </div>
 </template>
 
@@ -41,6 +22,7 @@ composition Заменяет data, methods, computed, watch
 
 // eslint-disable-next-line no-unused-vars
 import { ref, reactive, toRefs, computed, watch } from "vue";
+import FrameworkInfo from "@/FrameworkInfo";
 
 export default {
   setup() {
@@ -50,67 +32,16 @@ export default {
       version: 3,
     });
 
-    const framework2 = ref({
-      name2: "name2",
-      version2: 23,
-    });
-
-    const name3 = ref("name3");
-
-    const formInputs = reactive({
-      fName: "",
-      lName: "",
-    });
-    watch(formInputs, (val) => {
-      console.log("keys:", Object.keys(val));
-      console.log(val);
-      console.log(val.lName);
-    });
-
-    const firstName = ref("");
-    watch(firstName, (newVal) => {
-      // console.log(newVal);
-      console.log(newVal);
-    });
-
-    // инициализация ref на Dom
-    const textInput = ref(null);
-
     function changeInfo() {
       framework.name = "VUE JS changed";
-      framework2.value.name2 = "Name2 changed";
-      name3.value = "name3 changed";
-    }
-
-    const doubleVer = computed(() => {
-      return `<<${name3.value}>>`;
-    });
-
-    watch([doubleVer, name3], (newVal, oldVal) => {
-      console.log(newVal, oldVal);
-    });
-
-    const doubleFrame = computed(() => `<<${framework.name}>>`);
-
-    function handleInpVal() {
-      // textInput.value - указатель на DOM Node input
-      console.log(textInput.value.value);
     }
 
     // то что в return будет доступно в шаблоне
     return {
       ...toRefs(framework), // на выходе name + version в реактивном виде
-      framework2,
-      name3,
-
       change: changeInfo,
-      doubleVer,
-      doubleFrame,
-      textInput,
-      handleInpVal,
-      firstName,
-      formInputs,
     };
   },
+  components: { FrameworkInfo },
 };
 </script>
---
+++ src/FrameworkInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <p>
+      Название: <strong>{{ name }}</strong>
+    </p>
+    <p>
+      Версия: <strong>{{ version }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrameworkInfo",
+  props: ["name", "version"],
+  computed: {},
+};
+</script>
+
+<style scoped></style>
