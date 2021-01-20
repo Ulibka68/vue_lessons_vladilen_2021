b044dd3b2b90f8ae9af4164d9f660792e8b9e80d
Ulibka68 <gena68@inbox.ru>
2021-01-20 07:57:08 +0300

    12 Работа с Composition API   15 Пример: Переиспользование     добавлен обыычный AppAlert.vue

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert" :class="type" @click="$emit('close')">
+    <p class="alert-title">{{ title }}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAlert",
+  props: ["title", "type"],
+  emits: ["close"],
+};
+</script>
+
+<style scoped></style>
--- src/pages/ReusablePage.vue
+++ src/pages/ReusablePage.vue
@@ -1,18 +1,30 @@
 <template>
+  <AppAlert
+    v-if="alert"
+    @close="close"
+    title="Работа с Composition"
+    type="primary"
+  ></AppAlert>
   <div class="card">
     <h1>Reusable</h1>
 
-    <button class="btn primary">Показать сообщение</button>
+    <button class="btn primary" @click="toggle">
+      {{ alert ? "Скрыть" : "Показать" }} сообщение
+    </button>
     <button class="btn" @click="navigateHome">Перейти на главную</button>
   </div>
 </template>
 
 <script>
 import { useRouter, useRoute } from "vue-router";
+import { ref } from "vue";
+import AppAlert from "@/components/AppAlert";
 
 export default {
   name: "ReusablePage",
   setup() {
+    const alert = ref(false);
+
     const router = useRouter();
     const route = useRoute();
 
@@ -21,10 +33,22 @@ export default {
       router.push({ name: "home" });
     }
 
+    const toggle = () => {
+      alert.value = !alert.value;
+    };
+
+    const close = () => {
+      alert.value = false;
+    };
+
     return {
       navigateHome,
+      alert,
+      toggle,
+      close,
     };
   },
+  components: { AppAlert },
 };
 </script>
 
