638afaa591a27caed1d5414901a041fcd9b8349d
Ulibka68 <gena68@inbox.ru>
2021-01-24 11:46:49 +0300

    12 Работа с Composition API   Курсовая работа. Фриланс биржа     решение от Владилена first commit

---
+++ fbconf.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMtjFsvLly0Tip8ZKKtNNbHVyB2jAVxxU",
+  authDomain: "vue3-freelance-dz3-vlasov.firebaseapp.com",
+  databaseURL: "https://vue3-freelance-dz3-vlasov-default-rtdb.firebaseio.com",
+  projectId: "vue3-freelance-dz3-vlasov",
+  storageBucket: "vue3-freelance-dz3-vlasov.appspot.com",
+  messagingSenderId: "379729178514",
+  appId: "1:379729178514:web:5d6c1c2200aef285345b4b",
+};
