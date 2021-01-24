6e88d8dce50692d67960d1fec848a3f885f6c2ca
Ulibka68 <gena68@inbox.ru>
2021-01-24 12:09:15 +0300

    13 Vue на TypeScript   2 Как перевести существующий проект на TypeScript     first commit

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # Курс VUE 3 от Владилен
 https://vladilen.ru/pl/teach/control/lesson/view?id=187681124
+
+## 13 Vue на TypeScript
+
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@comp": path.resolve(__dirname, "./src/components"),
+        "@auth": path.resolve(__dirname, "./src/components/auth"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+      },
+      // extensions: [".js", ".vue", ".ts",".json"],
+    },
+  },
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false
+    }
+  }
+};
