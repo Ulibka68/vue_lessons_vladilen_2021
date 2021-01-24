f911fdbb4bc5f7f497b788bab6fdc5caf0ce3728
Ulibka68 <gena68@inbox.ru>
2021-01-24 19:09:42 +0300

    13 Vue на TypeScript   4 Начало работы с TypeScript     добавили метод на кнопку

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,13 @@
   <div class="card">
     <h1>{{ title }}</h1>
 
-    <button class="btn">Открыть пользователей</button>
+    <button class="btn" @click.prevent="navigate">Открыть пользователей</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
 /*
 export default defineComponent({
   data() {
@@ -21,8 +22,13 @@ export default defineComponent({
 
 export default defineComponent({
   setup() {
+    const router = useRouter();
+
     return {
       title: "Как Vue работает с TypeScript?",
+      navigate: () => {
+        router.push("/users");
+      },
     };
   },
 });
