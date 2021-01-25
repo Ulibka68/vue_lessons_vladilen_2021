46e992c3fbf99aa0ad8a6cfb0ae580beb7d9e6e9
Ulibka68 <gena68@inbox.ru>
2021-01-25 09:58:58 +0300

    14 Практика. Создание приложения   3 Форма авторизации и валидация     добавлены yup, vee validate     логику работы с формой перенесли в отдельный файл - хук

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -26,62 +26,13 @@
 </template>
 
 <script lang="ts">
-// eslint-disable-next-line no-unused-vars
-import { computed, watch } from "vue";
 import { defineComponent } from "vue";
-import { useField, useForm } from "vee-validate";
-import * as yup from "yup";
+import { useLoginForm } from "@/use/login-form";
 
 export default defineComponent({
   name: "Auth",
   setup() {
-    const { handleSubmit, isSubmitting, submitCount } = useForm();
-
-    const { value: email, errorMessage: eErorr, handleBlur: eBlur } = useField(
-      "email",
-      yup
-        .string()
-        .trim()
-        .required("Пожалуйста введите email")
-        .email("Введите корректный адрес электронной почты")
-    );
-    const {
-      value: password,
-      errorMessage: pError,
-      handleBlur: pBlur,
-    } = useField(
-      "password",
-      yup
-        .string()
-        .trim()
-        .required()
-        .min(2, "Пароль не может быть менее 2 символов")
-    );
-
-    const isTooManyAttempts = computed(() => submitCount.value >= 3);
-
-    watch(isTooManyAttempts, (val) => {
-      if (val) {
-        setTimeout(() => {
-          submitCount.value = 0;
-        }, 1500);
-      }
-    });
-
-    // eslint-disable-next-line no-unused-vars
-    const onSubmit = handleSubmit((values) => {});
-
-    return {
-      email,
-      password,
-      eErorr,
-      eBlur,
-      pError,
-      pBlur,
-      onSubmit,
-      isSubmitting,
-      isTooManyAttempts,
-    };
+    return { ...useLoginForm() };
   },
 });
 </script>
