33ca1f0146144ab6f06b41427799f5a0e48d4373
Ulibka68 <gena68@inbox.ru>
2021-01-25 11:20:28 +0300

    14 Практика. Создание приложения   4 Механизмы авторизации     работает logout

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,16 +5,29 @@
       <li><router-link to="/">Заявки</router-link></li>
       <li><router-link to="/help">Помощь</router-link></li>
       <li><a href="#">Сообщения</a></li>
-      <li><a href="#">Выход</a></li>
+      <li><a href="#" @click.prevent="logout">Выход</a></li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "@/store";
 
 export default defineComponent({
   name: "TheNavbar",
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+
+    return {
+      logout: () => {
+        store.commit("auth/logout");
+        router.push("/auth");
+      },
+    };
+  },
 });
 </script>
 
