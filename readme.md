29e067dc60074fb0111807e1f50dd0662acebf6c
Ulibka68 <gena68@inbox.ru>
2021-01-28 00:57:10 +0300

    Демонстрация типизации VUEX   добавил заготовки store

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # Демонстрация типизации VUEX
 
+https://gist.github.com/soerenmartius/ad62ad59b991c99983a4e495bf6acb04
+
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,32 @@
-import { createStore } from "vuex";
+import { createStore, createLogger } from "vuex";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+import {
+  AuthModule,
+  Store as AuthStore,
+  State as AuthState
+} from "@/store/modules/auth_store_index";
+
+import {
+  DomainModule,
+  Store as DomainStore,
+  State as DomainState
+} from "@/store/modules/domain_store_index";
+
+export type State = {
+  auth: AuthState;
+  domain: DomainState;
+};
+
+export type Store = AuthStore<Pick<State, "auth">> &
+  DomainStore<Pick<State, "domain">>;
+
+export const store = createStore({
+  plugins: process.env.NODE_ENV === "production" ? [] : [createLogger()],
+  modules: { AuthModule, DomainModule }
 });
+
+export function useStore(): Store {
+  return store as Store;
+}
+
+export default store;
