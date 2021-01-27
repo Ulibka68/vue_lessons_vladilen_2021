93fe714352b6463e7eee6fb85ab26dc53f22f9b1
Ulibka68 <gena68@inbox.ru>
2021-01-27 17:52:20 +0300

    14 Практика. Создание приложения   15 Страница помощи

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item
+      v-for="q in questions"
+      :key="q.title"
+      :question="q"
+    ></question-item>
+  </app-page>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppPage from "@/components/ui/AppPage.vue";
+import QuestionItem from "../components/question/QuestionItem.vue";
+
+export default defineComponent({
+  name: "Help",
+  setup() {
+    const questions = [
+      {
+        title: "Что есть в данной системе?",
+        text:
+          "Тут есть все, для того, чтобы смотреть, какие заявки человек добавлял",
+      },
+      {
+        title: "Как тут работает авторизация?",
+        text: "Все реализовано через Firebase",
+      },
+    ];
+
+    return { questions };
+  },
+  components: { AppPage, QuestionItem },
+});
+</script>
