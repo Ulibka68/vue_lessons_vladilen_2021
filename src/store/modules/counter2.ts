fbf9ccc6ed17e4cf3bb042ef5bbea01d5d280a1e
Ulibka68 <gena68@inbox.ru>
2021-02-01 21:32:09 +0300

    Демонстрация типизации VUEX мой вариант   мутации типизировал2

--- src/store/modules/counter2.ts
+++ src/store/modules/counter2.ts
@@ -1,11 +1,9 @@
 import { MutationTree } from "vuex";
-import { strEnum } from "./utility-func";
-import { State as RootState } from "@/store";
 
 type MutationPayload = {
-  setOne: { type: "setOne" };
-  setAdd: { type: "setAdd"; addCnt: number };
-  setMulti: { type: "setMulti"; addCnt1: number; addCnt2: number };
+  setOne: void;
+  setAdd: { addCnt: number };
+  setMulti: { addCnt1: number; addCnt2: number };
 };
 
 type Mutations = {
@@ -21,14 +19,14 @@ export const state = {
 
 export type State = typeof state;
 
-export const mutations: Mutations = {
-  setOne(state: State, payload: { type: "setOne" }) {
+export const mutations: MutationTree<State> & Mutations = {
+  setOne(state, payload) {
     state.counter = 0;
   },
   setAdd(state, payload) {
-    state.counter = 0;
+    state.counter += payload.addCnt;
   },
-  setMulti(state,payload){
-    state.counter = 0;
+  setMulti(state, payload) {
+    state.counter += payload.addCnt1 + payload.addCnt2;
   }
 };
